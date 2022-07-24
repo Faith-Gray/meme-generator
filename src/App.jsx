06006cc0cde29431f378de675b3f7ffa50c30c3e
Faith <fhgray96@gmail.com>
2022-07-24 13:16:11 -0500

    add html and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Header from './Header'
+import Meme from './Meme'
 
 function App() {
 
   return (
-    <div className="App">
-
+    <div>
+      <Header />
+      <Meme />
     </div>
   )
 }
